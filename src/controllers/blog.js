7e37dd05bbae97c4585a1e4a253382b00087aea5
Mahdiiigholipour <mahdigholipourprogramming@gmail.com>
2025-07-21 13:52:20 +0330

    feat : created functions primary structure

---
+++ src/controllers/blog.js
@@ -0,0 +1,71 @@
+async function getArticles(req, res, next) {
+  try {
+    const articles = await BlogService.getArticles();
+    res.json();
+  } catch (err) {
+    next(err);
+  }
+}
+
+async function createArticle(req, res, next) {
+  try {
+    const { title, publishingDate, content } = req?.body;
+    const result = await BlogService.createArticle({
+      title,
+      publishingDate,
+      content,
+    });
+  } catch (err) {
+    next(err);
+  }
+}
+
+async function updateArtice(req, res, next) {
+  try {
+    const { id } = req?.params;
+    const { title, publishingDate, content } = req?.body;
+
+    const result = await BlogService.updateArticle(id, {
+      title,
+      publishingDate,
+      content,
+    });
+  } catch (err) {
+    next(err);
+  }
+}
+
+async function deleteArticle(req, res, next) {
+  try {
+    const { id } = req?.params;
+    const result = await BlogService.deleteArticle(id);
+  } catch (err) {
+    next(err);
+  }
+}
+
+async function adminPage(req, res, next) {
+  try {
+    const result = await BlogService.adminPage();
+  } catch (err) {
+    next(err);
+  }
+}
+
+async function showArticle(req, res, next) {
+  try {
+    const { id } = req?.params;
+    const article = await BlogService.showArticle();
+  } catch (err) {
+    next(err);
+  }
+}
+
+module.exports = {
+  adminPage,
+  createArticle,
+  deleteArticle,
+  getArticles,
+  showArticle,
+  updateArtice,
+};
