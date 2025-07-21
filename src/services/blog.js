9ee79c63b2325693de30ee3b7d36d6970abd43ba
Mahdiiigholipour <mahdigholipourprogramming@gmail.com>
2025-07-21 16:21:32 +0330

    feat(createArticle): developed primary method

--- src/services/blog.js
+++ src/services/blog.js
@@ -1,6 +1,26 @@
+const fs = require("fs");
+const path = require("path");
+
 async function getArticles() {}
 
-async function createArticle(data) {}
+async function createArticle(data) {
+  const article = {
+    title: data?.title || "Unknown article",
+    publishingDate: data?.publishingDate || Date.now(),
+    content: data?.content || "",
+  };
+
+  const result = fs.writeFile(
+    `/articles/${article.publishingDate}.json`,
+    JSON.stringify(article),
+    (err) => {
+      if (err) throw new Error("cant write file!");
+      return true;
+    }
+  );
+
+  return result;
+}
 
 async function updateArticle(id, data) {}
 
