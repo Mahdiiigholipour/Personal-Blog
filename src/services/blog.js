ea79442b74d957b6b41e28cd3c7c7f46576dea91
Mahdiiigholipour <mahdigholipourprogramming@gmail.com>
2025-07-21 14:52:15 +0330

    feat : created service methods primary structure

---
+++ src/services/blog.js
@@ -0,0 +1,11 @@
+async function getArticles() {}
+
+async function createArticle(data) {}
+
+async function updateArticle(id, data) {}
+
+async function deleteArticle(id) {}
+
+async function adminPage() {}
+
+async function showArticle(id) {}
