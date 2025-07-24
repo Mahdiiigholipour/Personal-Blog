86d44a47aad0fec07448f0331d16d478af7592ad
Mahdiiigholipour <mahdigholipourprogramming@gmail.com>
2025-07-24 16:22:10 +0330

    config express for apis

--- index.js
+++ index.js
@@ -5,13 +5,18 @@ const {
   exceptionHandler,
 } = require("./src/middleware/errorHandler");
 require("dotenv").config();
+
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(router);
 app.get("/", (req, res) => res.send("test route"));
 
 app.use(notfoundHandler);
 app.use(exceptionHandler);
+
 app.listen(process.env.PORT, () =>
   console.log(`server run on : http://127.0.0.1:${process.env.PORT}`)
 );
