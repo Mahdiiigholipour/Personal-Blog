3518720c38132416c6954d8bca42ce7e926d676d
Mahdiiigholipour <mahdigholipourprogramming@gmail.com>
2025-07-24 16:43:35 +0330

    set configs in express for ejs

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 app.use(router);
 app.get("/", (req, res) => res.send("test route"));
 
