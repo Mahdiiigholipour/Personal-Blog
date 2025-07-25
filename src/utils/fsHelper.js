f133cb185ccd539e6deb3dd78209a65ab0bd6e40
Mahdiiigholipour <mahdigholipourprogramming@gmail.com>
2025-07-25 14:53:59 +0330

    add : read an article method

--- src/utils/fsHelper.js
+++ src/utils/fsHelper.js
@@ -2,7 +2,12 @@ const fs = require("fs").promises;
 const path = require("path");
 
 const articlesDir = path.resolve(process.cwd(), "articles");
+
 async function readArticlesDir() {
   await fs.mkdir(articlesDir, { recursive: true });
   return fs.readdir(articlesDir);
+}
+async function readArticleFile(filename) {
+  const filePath = path.resolve(articlesDir, filename);
+  return fs.readFile(filePath, "utf8");
 }
