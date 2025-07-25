6a297c292e786fe8beaaf030d970cb65577a3aa1
Mahdiiigholipour <mahdigholipourprogramming@gmail.com>
2025-07-25 14:21:29 +0330

    add admin username and password variables in env file and config dir

--- src/config/index.js
+++ src/config/index.js
@@ -2,4 +2,6 @@ require("dotenv").config();
 
 module.exports = {
   PORT: process.env.PORT || 3000,
+  ADMIN_USER: process.env.ADMIN_USER || "admin",
+  ADMIN_PASS: process.env.ADMIN_PASS || "admin",
 };
