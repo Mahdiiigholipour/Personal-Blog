8b69e01d70387757b627311ea5ce41797874a55b
Mahdiiigholipour <mahdigholipourprogramming@gmail.com>
2025-07-25 15:20:38 +0330

    refactored router

---
+++ src/blog/router.js
@@ -0,0 +1,47 @@
+const controller = require("./controller.js");
+const basicAuth = require("../middleware/basicAuth.js");
+const validate = require("../utils/validator.js");
+
+const router = require("express").Router();
+// Guest routes
+router.get("/", controller.getArticles);
+router.get(
+  "/article/:id",
+  [param("id").isNumeric()],
+  validate,
+  controller.getOneArticle
+);
+
+// Admin routes
+router.use(basicAuth);
+router.get("/admin/dashboard", controller.dashboardPage);
+router
+  .route("/admin/add")
+  .get(controller.addPage)
+  .post(
+    [
+      body("title").notEmpty(),
+      body("publishingDate").isNumeric(),
+      body("content").notEmpty(),
+    ],
+    validate,
+    controller.createArticle
+  );
+
+router
+  .route("/admin/edit/:id")
+  .get(controller.editPage)
+  .put(
+    [
+      param("id").isNumeric(),
+      body("title").optional().notEmpty(),
+      body("publishingDate").optional().isNumeric(),
+      body("content").optional().notEmpty(),
+    ],
+    validate,
+    controller.updateArticle
+  );
+
+router.delete("admin/delete/:id", controller.deleteArticle);
+
+module.exports = router;
