09753a0253d1ba2136591d9bb67a197deede73f9
Mahdiiigholipour <mahdigholipourprogramming@gmail.com>
2025-07-25 15:08:33 +0330

    add : validate mehtod

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -1,1 +1,18 @@
-function validate(req, res, next) {}
+const { validationResult } = require("express-validator");
+
+function validate(req, res, next) {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    const extractedErrors = errors.array().map((err) => ({
+      field: err.param,
+      message: err.msg,
+    }));
+
+    return res.status(400).json({ errors: extractedErrors });
+  }
+
+  next();
+}
+
+module.exports = validate;
