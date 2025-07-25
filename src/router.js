89bf1fccf2b7df794e420799aa5c570443ccd099
Mahdiiigholipour <mahdigholipourprogramming@gmail.com>
2025-07-25 12:02:05 +0330

    add(router): added get routes for rendering pages

--- src/router.js
+++ src/router.js
@@ -1,20 +1,25 @@
 const {
   getArticles,
   createArticle,
-  adminPage,
+  dashboardPage,
   getOneArticle,
   updateArtice,
   deleteArticle,
+  addPage,
+  editPage,
 } = require("./blog/controller.js");
 const authMiddleware = require("./middleware/auth");
 
 const router = require("express").Router();
 
-router.get("/home", getArticles);
 router.post("/new", createArticle);
-router.get("/admin", authMiddleware, adminPage);
-router.get("/article/:publishingDate", getOneArticle);
 router.put("/edit/:publishingDate", updateArtice);
 router.delete("/delete/:publishingDate", deleteArticle);
+router.get("/home", getArticles);
+router.get("/article/:publishingDate", getOneArticle);
+router.use(authMiddleware);
+router.get("/admin/dashboard", dashboardPage);
+router.get("/admin/add", addPage);
+router.get("/admin/edit", editPage);
 
 module.exports = router;
